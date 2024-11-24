aae82fc2ea12391b7ea9277e17ab332e342df59d
Awenes <preciousawe972@gmail.com>
2024-11-24 20:27:50 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import {VendorDashboard} from './pages/VendorDashboard/VendorDashboard'
+ 
+
+function App() {
+
+  return (
+    <div>
+      <VendorDashboard />
+    </div>
+  )
+}
+
+export default App
